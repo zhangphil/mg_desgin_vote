bac5c164903464aced97ed0532275c4aca5e8332
zhangphil <zhang.fei@msn.com>
2016-06-24 17:45:31 +0800

    更新

--- migu_preview/src/main/java/migu/demo/MyDialogFragment.java
+++ migu_preview/src/main/java/migu/demo/MyDialogFragment.java
@@ -1,4 +1,4 @@
-﻿package migu.demo;
+package migu.demo;
 
 
 import java.util.ArrayList;
@@ -39,21 +39,21 @@ public class MyDialogFragment extends DialogFragment {
 
 	private Activity activity;
 	private	BarChart mBarChart;
-	
+
 	private	int msg_what=0x0a2;
-	
+
 	private	ArrayList<PlanResult> voteResult;
-	
-	private	Handler handler = new Handler() {  
-        public void handleMessage(Message msg) {   
-             //super.handleMessage(msg); 
-        	Log.d("绘图", "开始设置数据");
-        	setData(mBarChart,voteResult, Utils.readAssetsFileCount(activity));
-        	//mBarChart.notifyDataSetChanged();
-        	mBarChart.invalidate();
-        	
-        }   
-	};  
+
+	private	Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+			//super.handleMessage(msg);
+			Log.d("绘图", "开始设置数据");
+			setData(mBarChart,voteResult, Utils.readAssetsFileCount(activity));
+			//mBarChart.notifyDataSetChanged();
+			mBarChart.invalidate();
+
+		}
+	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -71,7 +71,7 @@ public class MyDialogFragment extends DialogFragment {
 		mBarChart = (BarChart) view.findViewById(R.id.bar_chart);
 
 		setBarChartStyle(mBarChart);
-		
+
 		//new MyAsyncTask().execute();
 		queryData();
 		// 制作数据点。
@@ -83,7 +83,7 @@ public class MyDialogFragment extends DialogFragment {
 
 			}
 		});
-		
+
 		return builder.create();
 	}
 
@@ -147,7 +147,7 @@ public class MyDialogFragment extends DialogFragment {
 		public	int vote_count=0;
 		public	int plan_seq;
 	}
-	
+
 	private	boolean	hasPlanSeq(ArrayList<PlanResult> lists,int seq){
 		for(int i=0;i<lists.size();i++){
 			PlanResult pr=lists.get(i);
@@ -155,10 +155,10 @@ public class MyDialogFragment extends DialogFragment {
 				return	true;
 			}
 		}
-		
+
 		return	false;
 	}
-	
+
 	private ArrayList<PlanResult> readJSON(JSONArray jsonArray) throws Exception {
 		Log.d("json", jsonArray.toString());
 
@@ -170,37 +170,37 @@ public class MyDialogFragment extends DialogFragment {
 			String s=jo.getString("results");
 
 			//for (int j = 0; j < ja.length(); j++) {
-				//String s = ja.getString(j);
+			//String s = ja.getString(j);
 
-				int num = Integer.parseInt(s);
-				//Log.d("投票", num + "");
-				boolean b=hasPlanSeq(results, num);
-				
-				if(!b){
-					PlanResult pr=new PlanResult();
-					pr.plan_seq=num;
-					pr.vote_count=1;		
-					results.add(pr);
-					//Log.d("新加方案"+pr.plan_seq, pr.count+"");
+			int num = Integer.parseInt(s);
+			//Log.d("投票", num + "");
+			boolean b=hasPlanSeq(results, num);
+
+			if(!b){
+				PlanResult pr=new PlanResult();
+				pr.plan_seq=num;
+				pr.vote_count=1;
+				results.add(pr);
+				//Log.d("新加方案"+pr.plan_seq, pr.count+"");
+			}
+			else{
+				for(int k=0;k<results.size();k++){
+					PlanResult pr=results.get(k);
+					if(pr.plan_seq==num){
+						PlanResult pr_new=new PlanResult();
+						pr_new.plan_seq=num;
+						pr_new.vote_count=pr.vote_count+1;
+						results.remove(k);
+						results.add(pr_new);
+
+						//Log.d("调整方案"+pr_new.plan_seq, pr_new.count+"");
+						break;
+					}
 				}
-				else{
-					for(int k=0;k<results.size();k++){
-						PlanResult pr=results.get(k);
-						if(pr.plan_seq==num){
-							PlanResult pr_new=new PlanResult();
-							pr_new.plan_seq=num;
-							pr_new.vote_count=pr.vote_count+1;
-							results.remove(k);
-							results.add(pr_new);
-							
-							//Log.d("调整方案"+pr_new.plan_seq, pr_new.count+"");
-							break;
-						}
-					}					
-				}
+			}
 			//}
 		}
-		
+
 		/*
 		for(int i=0;i<results.size();i++){
 			PlanResult pr= results.get(i);
@@ -226,11 +226,11 @@ public class MyDialogFragment extends DialogFragment {
 				PlanResult pr=results.get(j);
 				if(pr.plan_seq==(i+1)){
 					yVals.add(new BarEntry(pr.vote_count, i));
-					
+
 					break;
 				}
 			}
-			
+
 			//yVals.add(new BarEntry(val, i));
 		}
 
@@ -249,7 +249,7 @@ public class MyDialogFragment extends DialogFragment {
 
 			@Override
 			public String getFormattedValue(float value, Entry entry, int dataSetIndex,
-					ViewPortHandler viewPortHandler) {
+											ViewPortHandler viewPortHandler) {
 
 				// DecimalFormat decimalFormat = new DecimalFormat(".0");
 				String s = ((int)value)+"票";
@@ -275,7 +275,7 @@ public class MyDialogFragment extends DialogFragment {
 
 		mBarChart.setData(mBarData);
 	}
-	
+
 	/*
 	private	class	MyAsyncTask	extends	AsyncTask{
 
@@ -283,13 +283,13 @@ public class MyDialogFragment extends DialogFragment {
 		protected Object doInBackground(Object... params) {
 
 			//queryData();
-			
+
 			return null;
 		}
-		
+
 		@Override
 		protected void onPostExecute(Object result) {
-			
+
 		}
 	}
 	*/
